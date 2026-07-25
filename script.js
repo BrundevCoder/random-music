da2ca89e78298cd190ea23645b9cdd1c1b262b26
Bruno Diniz <brunodevdrums@gmail.com>
2026-07-25 21:18:10 +0100

    added button to reload, started js by adding some musics data

---
+++ script.js
@@ -0,0 +1,8 @@
+const musicContainer = document.getElementById("s-container");
+const reloadButton = document.getElementById("reloadButton");
+
+const musics = [
+    {"Song": "Burger", "Artist": "bbno$", "Id": "3UbEemDEz6b6l5EBiswULJ"},
+    {"Song": "Bad", "Artist": "Michael Jackson", "Id": "2gSNBigeWMVtY3QBIvPAEc"}
+]
+
